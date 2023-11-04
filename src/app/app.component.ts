7478f8ad2cee9ec1e9d84e3c84bb0ce68ebead7a
Adnan Nizami <Anizami1217@gmail.com>
2023-11-03 22:37:59 -0700

    Final Touch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = "Adnan Nizami | Dev";
+export class AppComponent implements AfterViewInit {
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faFile = faFile;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    let skillsObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("skills-show");
+        }
+      });
+    });
+
+    let skills = document.querySelectorAll(".skills-hidden");
+    skills.forEach((element) => skillsObserver.observe(element));
+
+    let projectObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("project-show");
+        }
+      });
+    });
+
+    let projects = document.querySelectorAll(".project-hidden");
+    projects.forEach((element) => projectObserver.observe(element));
+  }
+
+  scrollIntoView(element: HTMLElement) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    MatButtonToggleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
